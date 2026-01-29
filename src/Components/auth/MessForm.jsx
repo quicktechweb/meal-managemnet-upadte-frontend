b3b6fc86d5c5606f1e310f5d61d9ac96543a93b0
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2026-01-29 17:29:21 +0600

    stepper form added

--- src/Components/auth/MessForm.jsx
+++ src/Components/auth/MessForm.jsx
@@ -1,174 +1,236 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 import { Link } from "react-router-dom";
+import Stepper from "./Stepper";
 
 const MessForm = () => {
+  const [step, setStep] = useState(1);
   const [passwordShow, setPasswordShow] = useState(false);
-  const [totalMembers, setTotalMembers] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({ totalMembers, members });
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const nextStep = async () => {
+    const valid = await trigger();
+    if (valid) setStep(step + 1);
+  };
+
+  const prevStep = () => setStep(step - 1);
+
+  const onSubmit = (data) => {
+    console.log("FORM DATA 👉", data);
   };
 
   return (
-    <form onSubmit={handleSubmit} className=" mx-auto space-y-3  rounded-lg">
-      {/* Basic Info */}
-      <FloatingInput label="Username" type="text" />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col  space-y-4 w-full"
+    >
+      <Stepper step={step} />
 
-      <FloatingInput label="Email" type="email" />
+      {/* ================= STEP 1 ================= */}
+      {step === 1 && (
+        <>
+          <FloatingInput
+            label="Username"
+            error={errors.username}
+            {...register("username", { required: "Username required" })}
+          />
 
-      {/* Password */}
-      <div className="relative">
-        <input
-          type={passwordShow ? "text" : "password"}
-          placeholder=" "
-          className="peer w-full border border-gray-200 rounded-md px-3 h-[50px] text-lg
-    focus:outline-none focus:border-black transition-all"
-        />
-        <FloatingLabel text="Password" />
-        <div
-          onClick={() => setPasswordShow(!passwordShow)}
-          className="absolute right-4 top-1/2 -translate-y-1/2 text-2xl cursor-pointer"
-        >
-          {passwordShow ? <IoMdEyeOff /> : <IoMdEye />}
-        </div>
-      </div>
+          <FloatingInput
+            label="Email"
+            type="email"
+            error={errors.email}
+            {...register("email", { required: "Email required" })}
+          />
 
-      <FloatingInput label="Phone Number" type="tel" />
+          {/* Password */}
+          <div className="relative w-full">
+            <input
+              type={passwordShow ? "text" : "password"}
+              placeholder=" "
+              {...register("password", {
+                required: "Password required",
+                minLength: 6,
+              })}
+              className="peer w-full border border-gray-300 rounded-md px-3 h-[50px]"
+            />
+            <FloatingLabel text="Password" />
+            <div
+              onClick={() => setPasswordShow(!passwordShow)}
+              className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
+            >
+              {passwordShow ? <IoMdEyeOff /> : <IoMdEye />}
+            </div>
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
 
-      <FloatingInput label="Address" type="text" />
+          <FloatingInput
+            label="Phone Number"
+            {...register("phone", { required: "Phone required" })}
+            error={errors.phone}
+          />
 
-      {/* Total Members */}
-      <div className="relative">
-        <input
-          type="number"
-          min="1"
-          value={totalMembers}
-          onChange={(e) => setTotalMembers(e.target.value)}
-          placeholder=" "
-          className="peer w-full border border-gray-200 rounded-md px-3 h-[50px] text-lg
-    focus:outline-none focus:border-black transition-all"
-        />
-        <FloatingLabel text="Total  Member" />
-      </div>
-      <div className="flex flex-col gap-2">
-        <select
-          className="peer w-full border border-gray-200 rounded-md px-3 h-[50px] text-lg
-                focus:outline-none focus:border-black transition-all"
-          defaultValue=""
-        >
-          <option value="" disabled>
-            Select Name of the institute
-          </option>
+          <FloatingInput label="Address" {...register("address")} />
 
-          <option>Institute 1</option>
-        </select>
-      </div>
-
-      <div className="flex items-center gap-2.5 ">
-        <label className="relative inline-block w-[18px] h-[10px]">
-          <input type="checkbox" className="peer sr-only" />
-          {/* Track */}
-          <span
-            className="
-      absolute inset-0 rounded-full bg-black cursor-pointer
-      transition-all duration-300 ease-[cubic-bezier(0.175,0.885,0.32,1.275)]
-     peer-checked:bg-[#F26831]
-      peer-focus:ring-1 peer-focus:ring-[#ec7c4f]
-    "
-          ></span>
-
-          {/* Thumb */}
-          <span
-            className="
-      absolute top-0 left-0 h-[10px] w-[10px] bg-white rounded-full
-      shadow-md
-      transition-all duration-300 ease-[cubic-bezier(0.175,0.885,0.32,1.275)]
-      peer-checked:translate-x-[8px]
-    "
-          ></span>
-        </label>
-        <label htmlFor="electricity-bill">Electricity Bill</label>
-      </div>
-
-      <div className="flex items-center gap-2.5 ">
-        <label className="relative inline-block w-[18px] h-[10px]">
-          <input type="checkbox" className="peer sr-only" />
-          {/* Track */}
-          <span
-            className="
-      absolute inset-0 rounded-full bg-black cursor-pointer
-      transition-all duration-300 ease-[cubic-bezier(0.175,0.885,0.32,1.275)]
-      peer-checked:bg-[#F26831]
-      peer-focus:ring-1 peer-focus:ring-[#ec7c4f]
-    "
-          ></span>
-
-          {/* Thumb */}
-          <span
-            className="
-      absolute top-0 left-0 h-[10px] w-[10px] bg-white rounded-full
-      shadow-md
-      transition-all duration-300 ease-[cubic-bezier(0.175,0.885,0.32,1.275)]
-      peer-checked:translate-x-[8px]
-    "
-          ></span>
-        </label>
-        <label htmlFor="electricity-bill">Staff Bill</label>
-      </div>
-
-      {/* <div className="flex items-center gap-2.5 ">
-        <input type="checkbox" name="staff-bill" />
-        <label htmlFor="staff-bill">Staff Bill</label>
-      </div> */}
-
-      {/* SIGN UP */}
-      <button
-        type="submit"
-        className="w-full py-3 cursor-pointer bg-black text-white text-xl rounded-lg"
-      >
-        Sign Up
-      </button>
-
-      <div>
-        <div className="mb-1 mb:mb-2 text-sm md:text-[18px] flex items-center gap-2">
-          <p>Already have an account?</p>{" "}
-          <Link
-            className="text-[rgba(50,100,245,0.90)] font-semibold"
-            to={"/login"}
+          <button
+            type="button"
+            onClick={nextStep}
+            className="w-full cursor-pointer bg-black text-white py-3 rounded-lg"
           >
-            Login
-          </Link>
-        </div>
+            Next
+          </button>
+        </>
+      )}
+
+      {/* ================= STEP 2 ================= */}
+      {step === 2 && (
+        <>
+          <p className="font-semibold">Select Kitchen Type</p>
+          <div className="flex items-center">
+            <div className="flex items-center gap-2 w-full">
+              <label className="switch !text-xs">
+                <input
+                  type="radio"
+                  value="al-abadin"
+                  {...register("kitchen", { required: "Select kitchen" })}
+                  className="sr-only"
+                />
+                <span className="slider"></span>
+              </label>
+              <p>Al Abadin Kitchen</p>
+            </div>
+            <div className="flex items-center gap-2 w-full">
+              <label className="switch !text-xs">
+                <input
+                  type="radio"
+                  value="user"
+                  {...register("kitchen", { required: "Select kitchen" })}
+                  className="sr-only"
+                />
+                <span className="slider"></span>
+              </label>
+              <p>User Kitchen</p>
+            </div>
+          </div>
+
+          {errors.kitchen && (
+            <p className="text-red-500 text-sm">{errors.kitchen.message}</p>
+          )}
+
+          <div className="flex gap-2">
+            <button
+              type="button"
+              onClick={prevStep}
+              className="w-full border cursor-pointer py-3 rounded-lg"
+            >
+              Back
+            </button>
+            <button
+              type="button"
+              onClick={nextStep}
+              className="w-full bg-black cursor-pointer text-white py-3 rounded-lg"
+            >
+              Next
+            </button>
+          </div>
+        </>
+      )}
+
+      {/* ================= STEP 3 ================= */}
+      {step === 3 && (
+        <>
+          <p className="font-semibold">Select Service</p>
+          <div className="flex items-center">
+            <div className="flex items-center gap-2 w-full">
+              <label className="switch !text-xs">
+                <input
+                  type="radio"
+                  value="al-abadin"
+                  {...register("service", { required: "Select Service" })}
+                  className="sr-only"
+                />
+                <span className="slider"></span>
+              </label>
+              <p>Per Meal</p>
+            </div>
+            <div className="flex items-center gap-2 w-full">
+              <label className="switch !text-xs">
+                <input
+                  type="radio"
+                  value="user"
+                  {...register("service", { required: "Select Service" })}
+                  className="sr-only"
+                />
+                <span className="slider"></span>
+              </label>
+              <p>Per Student</p>
+            </div>
+          </div>
+
+          {errors.service && (
+            <p className="text-red-500 text-sm">{errors.service.message}</p>
+          )}
+
+          <div className="flex gap-2">
+            <button
+              type="button"
+              onClick={prevStep}
+              className="w-full border cursor-pointer py-3 rounded-lg"
+            >
+              Back
+            </button>
+            <button
+              type="submit"
+              className="w-full cursor-pointer bg-black text-white py-3 rounded-lg"
+            >
+              Sign Up
+            </button>
+          </div>
+        </>
+      )}
+
+      <div className="text-sm flex gap-2">
+        <p>Already have an account?</p>
+        <Link to="/login" className="text-blue-600 font-semibold">
+          Login
+        </Link>
       </div>
     </form>
   );
 };
 
 export default MessForm;
-
-/* ---------- floating input ---------- */
-
-const FloatingInput = ({ label, type }) => (
-  <div className="relative">
-    <input
-      type={type}
-      placeholder=" "
-      className="peer w-full border border-gray-200 rounded-md px-3 h-[50px] text-lg
-    focus:outline-none focus:border-black transition-all"
-    />
-    <FloatingLabel text={label} />
-  </div>
+const FloatingInput = React.forwardRef(
+  ({ label, type = "text", error, ...rest }, ref) => (
+    <div className="relative w-full">
+      <input
+        ref={ref}
+        type={type}
+        placeholder=" "
+        {...rest}
+        className="peer w-full border border-gray-300 rounded-md px-3 h-[50px]"
+      />
+      <FloatingLabel text={label} />
+      {error && <p className="text-red-500 text-sm mt-1">{error.message}</p>}
+    </div>
+  ),
 );
-
 const FloatingLabel = ({ text }) => (
   <label
     className="absolute left-3 bg-white px-1 text-gray-500 transition-all
     top-1/2 -translate-y-1/2
     peer-focus:top-1 peer-focus:text-xs
-    peer-not-placeholder-shown:top-1 peer-not-placeholder-shown:text-xs
-    pointer-events-none"
+    peer-not-placeholder-shown:top-1 peer-not-placeholder-shown:text-xs"
   >
     {text}
   </label>
