c9acafaf7be5d7faa5b72bccbaba80e6fa873f1c
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2026-02-19 15:00:27 +0600

    rich editor data show

--- src/Pages/FrontEnd/admin/ChooseUsLists.jsx
+++ src/Pages/FrontEnd/admin/ChooseUsLists.jsx
@@ -62,7 +62,10 @@ const ChooseUsLists = () => {
 
                 {/* Description */}
                 <td className="px-3 md:px-4 py-3  border-r border-gray-300 text-sm text-gray-600 max-w-[250px]">
-                  <p className="line-clamp-2">{banner.description}</p>
+                  <p
+                    dangerouslySetInnerHTML={{ __html: banner.description }}
+                    className="[&_h1]:text-xl [&_h2]:text-xl [&_h3]:text-xl [&_h1]:font-bold [&_h2]:font-bold [&_h3]:font-bold [&_ul]:list-disc [&_ul]:list-inside [&_ol]:list-decimal [&_ol]:list-inside line-clamp-2"
+                  />
                 </td>
 
                 {/* Date */}
