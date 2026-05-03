75cfb4ba87fbdb749a708a5e5d636ab0535f79b5
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2026-05-03 15:37:37 +0600

    ecommerce sidebar added

--- src/Components/home/HomeSidebar.jsx
+++ src/Components/home/HomeSidebar.jsx
@@ -7,6 +7,9 @@ import { RiStarSmileLine } from "react-icons/ri";
 import { CiShop } from "react-icons/ci";
 import { useLayoutSwitch } from "../../providers/LayoutSwitchProvider";
 
+import { FaChevronDown, FaChevronRight } from "react-icons/fa";
+import { FaLaptop, FaMobileAlt, FaTshirt } from "react-icons/fa";
+import SubMenuPortal from "../SubMenuPortal";
 const mainNav = [
   {
     icon: <MdOutlineRestaurantMenu size={16} />,
@@ -52,17 +55,141 @@ const mealNav = [
   },
 ];
 
+export const categories = [
+  {
+    label: "Electronics",
+    icon: <FaMobileAlt />,
+    submenu: [
+      { label: "Mobile Phones", count: 120 },
+      { label: "Laptops", count: 80 },
+      { label: "Accessories", count: 150 },
+    ],
+  },
+  {
+    label: "Fashion",
+    icon: <FaTshirt />,
+    submenu: [
+      { label: "Men", count: 90 },
+      { label: "Women", count: 110 },
+      { label: "Kids", count: 70 },
+    ],
+  },
+  {
+    label: "Computers",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+  {
+    label: "Meat",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+  {
+    label: "Deli Meat",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+  {
+    label: "Ice Cream",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+  {
+    label: "Snacks",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+
+  {
+    label: "Coffee",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+  {
+    label: "Meat",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+  {
+    label: "Deli Meat",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+  {
+    label: "Ice Cream",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+  {
+    label: "Snacks",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+
+  {
+    label: "Coffee",
+    icon: <FaLaptop />,
+    submenu: [
+      { label: "Desktop", count: 40 },
+      { label: "Components", count: 65 },
+    ],
+  },
+];
+
 const HomeSidebar = ({ hideSidebar }) => {
   const [active, setActive] = useState("Home");
 
   const { selectedMenu } = useLayoutSwitch();
 
-  console.log(selectedMenu);
-
   const handleClick = (item) => {
     setActive(item.label);
     const el = document.getElementById(item.sectionId);
     if (el) el.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const [activeCategory, setActiveCategory] = useState(null);
+  const [submenuPos, setSubmenuPos] = useState({ top: 0 });
+  const [openMobileCat, setOpenMobileCat] = useState(null);
+
+  const handleMouseEnter = (e, cat) => {
+    if (window.innerWidth < 1024) return;
+    const rect = e.currentTarget.getBoundingClientRect();
+    setSubmenuPos({ top: rect.top });
+    setActiveCategory(cat);
+  };
+
+  const handleMouseLeave = () => {
+    setActiveCategory(null);
   };
 
   return (
@@ -97,6 +224,51 @@ const HomeSidebar = ({ hideSidebar }) => {
             />
           ))}
           <div className="h-px bg-gray-100 my-2 mx-2" />
+        </div>
+      )}
+
+      {selectedMenu === "e-commerce" && (
+        <div className="w-full h-full overflow-y-auto pt-2 lg:pt-22 p-4">
+          <h3 className="text-xl font-bold mb-4 hidden lg:block">
+            Shop by Category
+          </h3>
+
+          <ul className="space-y-2">
+            {categories.map((cat) => {
+              const isOpenMobile = openMobileCat?.label === cat.label;
+
+              return (
+                <li key={cat.label}>
+                  {/* Category Row */}
+                  <div
+                    onMouseEnter={(e) => handleMouseEnter(e, cat)}
+                    className="flex items-center justify-between px-4 py-3 rounded-lg cursor-pointer
+                            text-gray-700 hover:bg-[#c78436] hover:text-white transition"
+                  >
+                    <div className="flex items-center gap-3">
+                      <span className="text-lg">{cat.icon}</span>
+                      <span className="font-medium">{cat.label}</span>
+                    </div>
+
+                    {/* Icon change */}
+                    <span className="">
+                      {isOpenMobile ? <FaChevronDown /> : <FaChevronRight />}
+                    </span>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+
+          {activeCategory && (
+            <div className="block">
+              <SubMenuPortal
+                category={activeCategory}
+                position={submenuPos}
+                onClose={handleMouseLeave}
+              />
+            </div>
+          )}
         </div>
       )}
     </div>
